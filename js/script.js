24d75459f34acba25b649c1bbe9681fd41fc07fd
antonbodrov <james.bodrov@web.de>
2021-07-30 11:22:39 +0200

    cv

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-function get_today(){ 
-    var today = new Date(); 
+function get_today() {
+    var today = new Date();
 
-    var day = today.getDate(); // Tag 
+    var day = today.getDate(); // Tag
 
-    // Monatsangabe startet bei 0! 
-    var month = today.getMonth()+1; // Monat 
+    // Monatsangabe startet bei 0!
+    var month = today.getMonth() + 1; // Monat
 
-    var year = today.getFullYear(); // Jahr 
-    if(day < 10) { 
-            day = '0'+ day; 
-    } 
-    if(month < 10) { 
-            month = '0'+ month; 
-    } 
-    today = day + '.' + month + '.' + year; 
+    var year = today.getFullYear(); // Jahr
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+    today = ' ' + day + '.' + month + '.' + year;
 
-    return today; 
-} 
+    return today;
+}
 
 var today = get_today();
 
